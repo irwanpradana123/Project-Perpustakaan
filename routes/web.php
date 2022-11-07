427216e6d5b4b4a384e4db636e49c0d99a045273
ragakadir123 <irwan.pradana344@gmail.com>
2022-11-07 18:46:25 +0700

    menambahkan adminlte

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('petugas/dashboard');
+});
