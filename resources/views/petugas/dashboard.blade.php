7f2f7a0be5139349213c9a03369dc427879eff13
ragakadir123 <irwan.pradana344@gmail.com>
2022-11-07 21:27:39 +0700

    meanmbahkan admin-lte pada message src

--- resources/views/petugas/dashboard.blade.php
+++ resources/views/petugas/dashboard.blade.php
@@ -131,7 +131,7 @@
                                 <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="adminlte/dist/img/user1-128x128.jpg" alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 Is this template really for free? That's unbelievable!
@@ -147,7 +147,7 @@
                                 <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="adminlte/dist/img/user3-128x128.jpg" alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 You better believe it!
@@ -163,7 +163,8 @@
                                 <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="adminlte/dist/img/user1-128x128.jpg"
+                                alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 Working with AdminLTE on a great new app! Wanna join?
@@ -179,7 +180,8 @@
                                 <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="adminlte/dist/img/user3-128x128.jpg"
+                                alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 I would love to.
@@ -196,7 +198,8 @@
                         <ul class="contacts-list">
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user1-128x128.jpg" alt="User Avatar">
+                                    <img class="contacts-list-img" src="adminlte/dist/img/user1-128x128.jpg"
+                                        alt="User Avatar">
 
                                     <div class="contacts-list-info">
                                         <span class="contacts-list-name">
@@ -212,7 +215,8 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user7-128x128.jpg" alt="User Avatar">
+                                    <img class="contacts-list-img" src="adminlte/dist/img/user7-128x128.jpg"
+                                        alt="User Avatar">
 
                                     <div class="contacts-list-info">
                                         <span class="contacts-list-name">
@@ -227,7 +231,8 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user3-128x128.jpg" alt="User Avatar">
+                                    <img class="contacts-list-img" src="adminlte/dist/img/user3-128x128.jpg"
+                                        alt="User Avatar">
 
                                     <div class="contacts-list-info">
                                         <span class="contacts-list-name">
@@ -242,7 +247,8 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user5-128x128.jpg" alt="User Avatar">
+                                    <img class="contacts-list-img" src="adminlte/dist/img/user5-128x128.jpg"
+                                        alt="User Avatar">
 
                                     <div class="contacts-list-info">
                                         <span class="contacts-list-name">
@@ -257,7 +263,8 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user6-128x128.jpg" alt="User Avatar">
+                                    <img class="contacts-list-img" src="adminlte/dist/img/user6-128x128.jpg"
+                                        alt="User Avatar">
 
                                     <div class="contacts-list-info">
                                         <span class="contacts-list-name">
@@ -272,7 +279,8 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user8-128x128.jpg" alt="User Avatar">
+                                    <img class="contacts-list-img" src="adminlte/dist/img/user8-128x128.jpg"
+                                        alt="User Avatar">
 
                                     <div class="contacts-list-info">
                                         <span class="contacts-list-name">
