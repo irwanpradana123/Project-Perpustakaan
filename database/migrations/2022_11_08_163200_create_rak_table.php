ebc20c4bb5b88252060749d0ed7bcd74c28e310c
ragakadir123 <irwan.pradana344@gmail.com>
2022-11-08 23:44:13 +0700

    menambahkan rak baris dan kategori_id di table rak

--- database/migrations/2022_11_08_163200_create_rak_table.php
+++ database/migrations/2022_11_08_163200_create_rak_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('rak', function (Blueprint $table) {
             $table->id();
+            $table->string('rak');
+            $table->string('baris');
+            $table->foreignId('kategori_id');
             $table->timestamps();
         });
     }
